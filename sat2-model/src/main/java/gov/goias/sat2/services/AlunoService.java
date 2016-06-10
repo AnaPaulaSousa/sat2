d6362d6ad237356b5a261628932d66baf09f47f8
eder-bd <eder-bd@segplan.go.gov.br>
2016-06-10 16:03:49 -0300

    verificando id existente antes de excluir

--- sat2-model/src/main/java/gov/goias/sat2/services/AlunoService.java
+++ sat2-model/src/main/java/gov/goias/sat2/services/AlunoService.java
@@ -4,19 +4,17 @@ import gov.goias.excecao.InfraException;
 import gov.goias.historico.annotation.Historico;
 import gov.goias.sat2.entities.Aluno;
 import gov.goias.sat2.repositories.AlunoRepository;
-import javaslang.control.Option;
 import javaslang.control.Try;
 import org.jboss.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.data.domain.Example;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Map;
 import java.util.Optional;
+
+//import org.springframework.data.domain.Example;
 
 /**
  * Created by thiago-rs on 4/11/16.
@@ -28,65 +26,47 @@ public class AlunoService {
     @Autowired
     private AlunoRepository repository;
 
-    public Optional<Aluno> obterPorId(final Long id){
+    public Optional<Aluno> obterPorId(final Long id) {
         return Try.of(() -> repository.findById(id))
                 .onFailure(e -> new InfraException(e))
                 .get();
     }
 
-    @Transactional
+    @Transactional(readOnly = false, rollbackFor = RuntimeException.class)
     @Historico
-    public Aluno salvar(final Aluno aluno){
+    public Aluno salvar(final Aluno aluno) {
         return Try.of(() -> repository.save(aluno))
                 .onFailure(e -> new InfraException(e))
                 .get();
     }
 
-    @Transactional(noRollbackFor = EmptyResultDataAccessException.class)
+    @Transactional(readOnly = false, rollbackFor = RuntimeException.class)
     @Historico
-    public void remover(final Long id){
-        try{
-            repository.delete(id);
-        }catch(EmptyResultDataAccessException e){
-            log.debug(e);
-        }catch(Exception e){
-            new InfraException(e);
+    public void remover(final Long id) {
+        try {
+            repository.findById(id).ifPresent(aluno -> repository.delete(aluno));
+        } catch (Exception e) {
+            throw new InfraException(e);
         }
     }
 
-    public Page<Aluno> listarTodos(final PageRequest page){
+    public Page<Aluno> listarTodos(final PageRequest page) {
         return Try.of(() -> repository.findAll(page))
                 .onFailure(e -> new InfraException(e))
                 .get();
     }
 
-    public Page<Aluno> listarPaginado(final PageRequest page){
+    public Page<Aluno> listarPaginado(final PageRequest page) {
         return Try.of(() -> repository.findAll(page))
-            .onFailure(e -> new InfraException(e))
-            .get();
+                .onFailure(e -> new InfraException(e))
+                .get();
     }
 
-    public Long contarTodos(){
+    public Long contarTodos() {
         return Try.of(() -> repository.count())
                 .onFailure(e -> new InfraException(e))
                 .get();
     }
-
-//    public Long contarFitrados(final Aluno filtro) throws InfraException {
-//        final Example<Aluno> ex = Example.of(filtro);
-////        ExampleMatcher matcher = ExampleMatcher.matching().
-////        .withMatcher("firstname", endsWith())
-////                .withMatcher("lastname", startsWith().ignoreCase());
-////
-////        Example<Person> example = Example.of(new Person("Jon", "Snow"), matcher);
-////        repo.count(example);
-//        return Try.of(() -> repository.count(ex))
-//                .onFailure(e -> new InfraException(e))
-//                .get();
-//
-//    }
-
-
 
 
 }
