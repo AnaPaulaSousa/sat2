768bbb253e885f62ab7046b38e0f7568f70bd3b7
Thiago Rios de Siqueira <thiago-rs@segplan.go.gov.br>
2016-09-20 17:08:38 -0300

    Pacote de recursos para Jersey

--- sat2-app/src/main/webapp/js/messages.js
+++ sat2-app/src/main/webapp/js/messages.js
@@ -4,7 +4,7 @@ messages.factory('Messages', [
     function(){
 
         return {
-            salvoComSucesso: function (registro) { return `${registro} salvo com sucesso` },
+            salvoComSucesso: function (registro) { return '${registro} salvo com sucesso'},
             registroRemovido: "O registro foi removido",
             erroEfetuarOp: "Não foi possível efetuar a operação",
         };
